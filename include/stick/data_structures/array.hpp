7cba006db6035197ef53e46040469c150b483d77
stickm4n <jcgalindo.jcgh@gmail.com>
2021-12-29 20:04:18 -0500

    Created hybrid array class for both FIFO and LIFO usage.

---
+++ include/stick/data_structures/array.hpp
@@ -0,0 +1,403 @@
+/**
+ * Hybrid FIFO-LIFO array template. Can be used as a queue and a stack
+ * simultaneously.
+ * @brief Hybrid array template.
+ * @file array.hpp
+ * @author Julio C. Galindo (stickM4N)
+ */
+#if not defined(_stick_lib_data_structures_array_)
+#	define _stick_lib_data_structures_array_
+
+
+
+#	include "../defines/types.hpp"
+#	include "../memory/scoped_pointer.hpp"
+
+
+namespace stick {
+
+
+	template<typename type>
+	class array {
+
+		scoped_pointer<type> data;
+		size_t data_start = 0ul;
+		size_t data_size = 0ul;
+		size_t front_pool = 0ul;
+		size_t back_pool = 0ul;
+
+	protected:
+		/**
+		 * Possible cases to move the array when push-ing and pop-ing values.
+		 */
+		enum class move_decision_result : uint8_t {
+			expand_and_move_front,   /// Expand the array and move the front
+			                         /// part.
+			move_front,   /// Only move the front part.
+			expand_and_move_back,   /// Expand the array and move the back part.
+			move_back   /// Only move the back part.
+		};
+
+		/**
+		 * Find the best way to move the array to push or pop values.
+		 * @param [in] position Where the value is being push-ed or pop-ed.
+		 * @return move_decision_result of the most effective
+		 */
+		move_decision_result make_move_decision(size_t position) const noexcept;
+
+
+		/**
+		 * Get the position of the first valid value in the allocated memory.
+		 * @return Position of the first valid value in the allocated memory.
+		 */
+		size_t first_position() const noexcept;
+		/**
+		 * Get the position of the last valid value in the allocated memory.
+		 * @return Position of the last valid value in the allocated memory.
+		 */
+		size_t last_position() const noexcept;
+
+
+		/**
+		 * Begin iterator of the allocated memory.
+		 * @return Pointer to the first element allocated.
+		 */
+		type *allocated_begin() const noexcept;
+		/**
+		 * End iterator of the allocated memory.
+		 * @return Pointer to one element after the last one allocated.
+		 */
+		type *allocated_end() const noexcept;
+
+		/**
+		 * Reverse begin iterator of the allocated memory.
+		 * @return Pointer to the last allocated element.
+		 */
+		type *allocated_rbegin() const noexcept;
+		/**
+		 * Revere end iterator of the allocated memory.
+		 * @return Pointer to one element before the first allocated.
+		 */
+		type *allocated_rend() const noexcept;
+
+	public:
+		/**
+		 * Default constructor for empty array.
+		 */
+		array() noexcept = default;
+		/**
+		 * Create an array copying from a basic array.
+		 * @param [in] array Pointer to the array to copy from.
+		 * @param [in] length Number of elements to copy from the array.
+		 * @throw memory_error when copying from a nullptr.
+		 */
+		explicit array(const type *array, size_t length);
+		/**
+		 * Create an empty array with reserved space.
+		 * @param [in] size Number of elements to reserve space for.
+		 * @param [in] front_pool_length Extra element to reserve space at the
+		 * beginning of the array.
+		 * @param [in] back_pool_length Extra element to reserve space at the
+		 * end of the array.
+		 * @throw memory_error when copying from a nullptr or allocating 0 byte.
+		 */
+		explicit array(size_t size, size_t front_pool_length = 0ul,
+		               size_t back_pool_length = 0ul);
+		/**
+		 * Default copy constructor.
+		 * @param [in] array Array to copy from.
+		 */
+		array(const array &array) noexcept = default;
+		/**
+		 * Move constructor.
+		 * @param [in,out] array Array to move from and clear.
+		 */
+		array(array &&array) noexcept;
+
+		/**
+		 * Clear the array completely and its parameters. Also deallocates the
+		 * memory.
+		 */
+		~array() noexcept;
+
+
+		/**
+		 * Begin iterator.
+		 * @return Pointer to the first element.
+		 */
+		type *begin() const noexcept;
+		/**
+		 * End iterator.
+		 * @return Pointer to one element after the last.
+		 */
+		type *end() const noexcept;
+
+		/**
+		 * Reverse begin iterator.
+		 * @return Pointer to the last element.
+		 */
+		type *rbegin() const noexcept;
+		/**
+		 * Revere end iterator.
+		 * @return Pointer to one element before the first.
+		 */
+		type *rend() const noexcept;
+
+
+
+		/**
+		 * Get the number of valid elements in the array.
+		 * @return Number of valid elements.
+		 */
+		size_t size() const noexcept;
+		/**
+		 * Get the total number allocated elements in the array.
+		 * @return Number of allocated elements.
+		 */
+		size_t allocated_size() const noexcept;
+
+		/**
+		 * Get the number of front elements that can be pushed before
+		 * reallocating the array.
+		 * @return Number of free elements in the front.
+		 */
+		size_t available_front_pool() const noexcept;
+		/**
+		 * Get the number of back elements that can be pushed before
+		 * reallocating the array.
+		 * @return Number of free elements in the back.
+		 */
+		size_t available_back_pool() const noexcept;
+
+		/**
+		 * Check if the array is empty.
+		 * @return True if there are no valid elements, false otherwise.
+		 */
+		bool_t empty() const noexcept;
+
+
+		/**
+		 * Set the amount of elements to pad in the front on each reallocation.
+		 * @param [in] size Number of elements to pad front.
+		 * @return Self instance.
+		 */
+		array &set_front_pool(size_t size) noexcept;
+		/**
+		 * Set the amount of elements to pad in the back on each reallocation.
+		 * @param [in] size Number of elements to pad back.
+		 * @return Self instance.
+		 */
+		array &set_back_pool(size_t size) noexcept;
+
+
+		/**
+		 * Expand the allocated memory to fit more values preserving the current
+		 * ones.
+		 * @param [in] front_size Number of front elements to reserve.
+		 * @param [in] back_size Number of back elements to reserve.
+		 * @return Self instance.
+		 */
+		array &expand(size_t front_size, size_t back_size) noexcept;
+		/**
+		 * Expand the allocated memory to fit more values in the front of the
+		 * array preserving the current ones.
+		 * @param [in] size Number of front elements to reserve.
+		 * @return Self instance.
+		 */
+		array &expand_front(size_t size) noexcept;
+		/**
+		 * Expand the allocated memory to fit more values in the back of the
+		 * array preserving the current ones.
+		 * @param [in] size Number of back elements to reserve.
+		 * @return Self instance.
+		 */
+		array &expand_back(size_t size) noexcept;
+
+
+		/**
+		 * Reduce allocated memory preserving values in the array.
+		 * @param [in] front_size Number of elements to reduce from front pool.
+		 * Default = max front padding.
+		 * @param [in] back_size Number of elements to reduce from back pool.
+		 * Default = max back padding.
+		 * @return Self instance.
+		 */
+		array &compact(size_t front_size = -1ul,
+		               size_t back_size = -1ul) noexcept;
+		/**
+		 * Reduce allocated memory preserving values in the front of the array.
+		 * @param [in] size Number of elements to reduce from front pool.
+		 * Default = max back padding.
+		 * @return Self instance.
+		 */
+		array &compact_front(size_t size = -1ul) noexcept;
+		/**
+		 * Reduce allocated memory preserving values in the back of the array.
+		 * @param [in] size Number of elements to reduce from back pool. Default
+		 * = max back padding.
+		 * @return Self instance.
+		 */
+		array &compact_back(size_t size = -1ul) noexcept;
+
+
+		/**
+		 * Insert a value at a given position in the array.
+		 * @param [in] value Value to be inserted.
+		 * @param [in] position Position to insert the value in.
+		 * @throw out_of_range_error when inserting in an invalid position.
+		 * @return Self instance.
+		 */
+		array &push(const type &value, ssize_t position);
+		/**
+		 * Insert a value at a given position in the array.
+		 * @param [in] value Value to be inserted.
+		 * @param [in] position Position to insert the value in.
+		 * @throw out_of_range_error when inserting in an invalid position.
+		 * @return Self instance.
+		 */
+		array &push(const type &value, size_t position);
+		/**
+		 * Insert a value at a given position in the array.
+		 * @param [in] value Value to be inserted.
+		 * @param [in] position Position to insert the value in.
+		 * @throw out_of_range_error when inserting in an invalid position.
+		 * @return Self instance.
+		 */
+		array &push(const type &value, int32_t position);
+		/**
+		 * Insert a value at a given position in the array.
+		 * @param [in] value Value to be inserted.
+		 * @param [in] position Position to insert the value in.
+		 * @throw out_of_range_error when inserting in an invalid position.
+		 * @return Self instance.
+		 */
+		array &push(const type &value, uint32_t position);
+		/**
+		 * Insert a value at the beginning of the array.
+		 * @param [in] value Value to be inserted.
+		 * @return Self instance.
+		 */
+		array &push_front(const type &value) noexcept;
+		/**
+		 * Insert a value at the end of the array.
+		 * @param [in] value Value to be inserted.
+		 * @return Self instance.
+		 */
+		array &push_back(const type &value) noexcept;
+
+
+		/**
+		 * Remove the value located a given position in the array.
+		 * @param [in] value Value to be removed.
+		 * @param [in] position Position in which the value will be removed.
+		 * @throw out_of_range_error when removing at an invalid position.
+		 * @return Self instance.
+		 */
+		array &pop(ssize_t position);
+		/**
+		 * Remove the value located a given position in the array.
+		 * @param [in] value Value to be removed.
+		 * @param [in] position Position in which the value will be removed.
+		 * @throw out_of_range_error when removing at an invalid position.
+		 * @return Self instance.
+		 */
+		array &pop(size_t position);
+		/**
+		 * Remove the value located a given position in the array.
+		 * @param [in] value Value to be removed.
+		 * @param [in] position Position in which the value will be removed.
+		 * @throw out_of_range_error when removing at an invalid position.
+		 * @return Self instance.
+		 */
+		array &pop(int32_t position);
+		/**
+		 * Remove the value located a given position in the array.
+		 * @param [in] value Value to be removed.
+		 * @param [in] position Position in which the value will be removed.
+		 * @throw out_of_range_error when removing at an invalid position.
+		 * @return Self instance.
+		 */
+		array &pop(uint32_t position);
+		/**
+		 * Remove a value from the beginning of the array.
+		 * @param [in] value Value to be removed.
+		 * @return Self instance.
+		 */
+		array &pop_front();
+		/**
+		 * Remove a value from the end of the array.
+		 * @param [in] value Value to be removed.
+		 * @return Self instance.
+		 */
+		array &pop_back();
+
+
+		/**
+		 * Clear array values keeping its allocated size.
+		 * @return Self instance.
+		 */
+		array &clear() noexcept;
+
+
+		/**
+		 * Get the value at a given position. Negative integers start from the
+		 * last allocated value.
+		 * @param [in] position Position of the value to return.
+		 * @throw out_of_range_error when accessing a value outside the array.
+		 * @return The value stored in the given position.
+		 */
+		type &operator[](ssize_t position) const;
+		/**
+		 * Get the value at a given position.
+		 * @param [in] position Position of the value to return.
+		 * @throw out_of_range_error when accessing a value outside the array.
+		 * @return The value stored in the given position.
+		 */
+		type &operator[](size_t position) const;
+		/**
+		 * Get the value at a given position. Negative integers start from the
+		 * last allocated value. Exists for typical values.
+		 * @param [in] position Position of the value to return.
+		 * @throw out_of_range_error when accessing a value outside the array.
+		 * @return The value stored in the given position.
+		 */
+		type &operator[](int32_t position) const;
+		/**
+		 * Get the value at a given position. Exists for typical values.
+		 * @param [in] position Position of the value to return.
+		 * @throw out_of_range_error when accessing a value outside the array.
+		 * @return The value stored in the given position.
+		 */
+		type &operator[](uint32_t position) const;
+
+		/**
+		 * Default copy initializer.
+		 * @param [in] array Array to copy from.
+		 * @return Self instance.
+		 */
+		array &operator=(const array &array) noexcept = default;
+		/**
+		 * Move initializer.
+		 * @param [in] array Array to move from and clear.
+		 * @return Self instance.
+		 */
+		array &operator=(array &&array) noexcept;
+
+		/**
+		 * Ability to cast the array to basic array.
+		 * @return Pointer to the array of the valid values.
+		 */
+		operator type *() const noexcept;
+	};
+
+
+
+}   // namespace stick
+
+
+#	define _stick_lib_data_structures_array_impl_
+#	include "../data_structures/array_impl.hpp"
+#	undef _stick_lib_data_structures_array_impl_
+
+
+#endif   //_stick_lib_data_structures_array_
---
+++ include/stick/data_structures/array_impl.hpp
@@ -0,0 +1,391 @@
+/**
+ * Hybrid FIFO-LIFO array. Template implementations.
+ * @brief Hybrid array template implementations.
+ * @file array_impl.hpp
+ * @author Julio C. Galindo (stickM4N)
+ */
+#if defined(_stick_lib_data_structures_array_impl_)
+
+
+#	include "../error/out_of_range_error.hpp"
+#	include "../memory/management.hpp"
+#	include "../templates/reference.hpp"
+
+
+namespace stick {
+
+
+	template<typename type>
+	typename array<type>::move_decision_result
+	array<type>::make_move_decision(size_t position) const noexcept {
+
+		bool_t prefer_moving_back = this->size() - position <= position;
+
+		if (prefer_moving_back) {
+			if (this->available_back_pool() != 0ul)
+				return move_decision_result::move_back;
+			else if (this->available_front_pool() != 0ul)
+				return move_decision_result::move_front;
+		} else {
+			if (this->available_front_pool() != 0ul)
+				return move_decision_result::move_front;
+			else if (this->available_back_pool() != 0ul)
+				return move_decision_result::move_back;
+		}
+
+		if (prefer_moving_back) {
+			return move_decision_result::expand_and_move_back;
+		} else {
+			return move_decision_result::expand_and_move_front;
+		}
+	}
+
+
+	template<typename type>
+	size_t array<type>::first_position() const noexcept {
+		return this->data_start;
+	}
+	template<typename type>
+	size_t array<type>::last_position() const noexcept {
+		return this->data_start + this->data_size - 1;
+	}
+
+
+	template<typename type>
+	type *array<type>::allocated_begin() const noexcept {
+		return this->data.begin();
+	}
+	template<typename type>
+	type *array<type>::allocated_end() const noexcept {
+		return this->data.end();
+	}
+
+	template<typename type>
+	type *array<type>::allocated_rbegin() const noexcept {
+		return this->data.rbegin();
+	}
+	template<typename type>
+	type *array<type>::allocated_rend() const noexcept {
+		return this->data.rend();
+	}
+
+
+	template<typename type>
+	array<type>::array(const type *array, size_t length)
+	    : data(array, length), data_size(length) { }
+	template<typename type>
+	array<type>::array(size_t size, size_t front_pool_length,
+	                   size_t back_pool_length)
+	    : data(front_pool_length + size + back_pool_length),
+	      front_pool(front_pool_length), back_pool(back_pool_length),
+	      data_start(front_pool_length) { }
+	template<typename type>
+	array<type>::array(array &&array) noexcept {
+		this->data = move(array.data);
+		this->data_start = array.data_start;
+		this->data_size = array.data_size;
+		this->front_pool = array.front_pool;
+		this->back_pool = array.back_pool;
+
+		array.data_start = 0ul;
+		array.data_size = 0ul;
+		array.front_pool = 0ul;
+		array.back_pool = 0ul;
+	}
+
+	template<typename type>
+	array<type>::~array() noexcept {
+		this->data.~scoped_pointer();
+		this->data_start = 0ul;
+		this->data_size = 0ul;
+		this->front_pool = 0ul;
+		this->back_pool = 0ul;
+	}
+
+
+	template<typename type>
+	type *array<type>::begin() const noexcept {
+		return &this->data[this->first_position()];
+	}
+	template<typename type>
+	type *array<type>::end() const noexcept {
+		if (this->empty())
+			return this->begin();
+		else
+			return &this->data[this->last_position()];
+	}
+
+	template<typename type>
+	type *array<type>::rbegin() const noexcept {
+		if (this->empty())
+			return this->rend();
+		else
+			return &this->data[this->last_position()];
+	}
+	template<typename type>
+	type *array<type>::rend() const noexcept {
+		return &this->data[this->first_position()] - 1ul;
+	}
+
+
+	template<typename type>
+	size_t array<type>::size() const noexcept {
+		return this->data_size;
+	}
+	template<typename type>
+	size_t array<type>::allocated_size() const noexcept {
+		return this->data.allocated_elems();
+	}
+	template<typename type>
+
+	size_t array<type>::available_front_pool() const noexcept {
+		return this->first_position();
+	}
+	template<typename type>
+	size_t array<type>::available_back_pool() const noexcept {
+		return this->allocated_size() - this->last_position();
+	}
+
+	template<typename type>
+	bool_t array<type>::empty() const noexcept {
+		return this->size() == 0ul;
+	}
+
+
+	template<typename type>
+	array<type> &array<type>::set_front_pool(size_t size) noexcept {
+		this->front_pool = size;
+
+		return *this;
+	}
+	template<typename type>
+	array<type> &array<type>::set_back_pool(size_t size) noexcept {
+		this->back_pool = size;
+
+		return *this;
+	}
+
+
+	template<typename type>
+	array<type> &array<type>::expand(size_t front_size,
+	                                 size_t back_size) noexcept {
+		array<type> tempy(this->allocated_size(), front_size, back_size);
+
+		if (not this->empty())
+			for (const type &e : *this)
+				tempy.push_back(e);
+
+		*this = move(tempy);
+
+		return *this;
+	}
+	template<typename type>
+	array<type> &array<type>::expand_front(size_t size) noexcept {
+		return this->expand(size, 0ul);
+	}
+	template<typename type>
+	array<type> &array<type>::expand_back(size_t size) noexcept {
+		return this->expand(0ul, size);
+	}
+
+
+	template<typename type>
+	array<type> &array<type>::compact(size_t front_size,
+	                                  size_t back_size) noexcept {
+		if (this->available_front_pool() <= front_size)
+			front_size = 0ul;
+		else
+			front_size = this->available_front_pool() - front_size;
+
+		if (this->available_back_pool() <= back_size)
+			back_size = 0ul;
+		else
+			back_size = this->available_back_pool() - back_size;
+
+		if (front_size + this->size() + back_size != 0ul) {
+			array<type> tempy(this->size(), front_size, back_size);
+			for (const auto &e : *this)
+				tempy.push_back(e);
+
+			*this = move(tempy);
+		} else {
+			this->data.~scoped_pointer();
+			this->data_start = 0ul;
+			this->data_size = 0ul;
+		}
+
+		return *this;
+	}
+	template<typename type>
+	array<type> &array<type>::compact_front(size_t size) noexcept {
+		return this->compact(size, 0ul);
+	}
+	template<typename type>
+	array<type> &array<type>::compact_back(size_t size) noexcept {
+		return this->compact(0ul, size);
+	}
+
+
+	template<typename type>
+	array<type> &array<type>::push(const type &value, ssize_t position) {
+		if (position < 0l)
+			position = this->last_position() + position + 1l;
+
+		return this->push(value, static_cast<size_t>(position));
+	}
+	template<typename type>
+	array<type> &array<type>::push(const type &value, size_t position) {
+		if (this->size() < position)
+			throw out_of_range_error("Push-ing a value outside the array.");
+
+
+		switch (this->make_move_decision(position)) {
+
+		case move_decision_result::expand_and_move_back:
+			this->expand_back(this->back_pool + 1ul);
+		case move_decision_result::move_back:
+			move(this->begin() + position, this->begin() + position + 1ul,
+			     this->size() - position, false);
+			this->data_size++;
+			this->operator[](position) = value;
+
+			break;
+		case move_decision_result::expand_and_move_front:
+			this->expand_front(this->back_pool + 1ul);
+		case move_decision_result::move_front:
+			move(this->begin(), this->begin() - position - 1ul, position + 1ul,
+			     false);
+			this->data_start--;
+			this->data_size++;
+			this->operator[](position) = value;
+
+			break;
+		}
+
+		return *this;
+	}
+	template<typename type>
+	array<type> &array<type>::push(const type &value, int32_t position) {
+		return this->push(value, static_cast<ssize_t>(position));
+	}
+	template<typename type>
+	array<type> &array<type>::push(const type &value, uint32_t position) {
+		return this->push(value, static_cast<size_t>(position));
+	}
+	template<typename type>
+	array<type> &array<type>::push_front(const type &value) noexcept {
+		return this->push(value, 0ul);
+	}
+	template<typename type>
+	array<type> &array<type>::push_back(const type &value) noexcept {
+		return this->push(value, this->size());
+	}
+
+
+	template<typename type>
+	array<type> &array<type>::pop(ssize_t position) {
+		if (position < 0l)
+			position = this->last_position() + position + 1l;
+
+		return this->pop(static_cast<size_t>(position));
+	}
+	template<typename type>
+	array<type> &array<type>::pop(size_t position) {
+		if (this->size() <= position)
+			throw out_of_range_error("Pop-ing a value outside the array.");
+
+		switch (this->make_move_decision(position)) {
+
+		case move_decision_result::expand_and_move_back:
+		case move_decision_result::move_back:
+			move(this->begin() + position + 1ul, this->begin() + position,
+			     this->size() - position - 1ul, false);
+			break;
+		case move_decision_result::expand_and_move_front:
+		case move_decision_result::move_front:
+			move(this->begin(), this->begin() + 1ul, position, false);
+			this->data_start++;
+			break;
+		}
+		this->data_size--;
+
+		return *this;
+	}
+	template<typename type>
+	array<type> &array<type>::pop(int32_t position) {
+		return this->pop(static_cast<ssize_t>(position));
+	}
+	template<typename type>
+	array<type> &array<type>::pop(uint32_t position) {
+		return this->pop(static_cast<size_t>(position));
+	}
+	template<typename type>
+	array<type> &array<type>::pop_front() {
+		return this->pop(0ul);
+	}
+	template<typename type>
+	array<type> &array<type>::pop_back() {
+		return this->pop(-1ul);
+	}
+
+
+	template<typename type>
+	array<type> &array<type>::clear() noexcept {
+		this->data_size = 0ul;
+
+		return *this;
+	}
+
+
+	template<typename type>
+	type &array<type>::operator[](ssize_t position) const {
+		if (position < 0l)
+			position = this->last_position() + position + 1l;
+
+		return this->operator[](static_cast<size_t>(position));
+	}
+	template<typename type>
+	type &array<type>::operator[](size_t position) const {
+		if (this->size() <= position)
+			throw out_of_range_error("Accessing a value outside the array.");
+
+		return this->data[this->first_position() + position];
+	}
+	template<typename type>
+	type &array<type>::operator[](int32_t position) const {
+		return this->operator[](static_cast<ssize_t>(position));
+	}
+	template<typename type>
+	type &array<type>::operator[](uint32_t position) const {
+		return this->operator[](static_cast<size_t>(position));
+	}
+
+	template<typename type>
+	array<type> &array<type>::operator=(array &&array) noexcept {
+		this->data = move(array.data);
+		this->data_start = array.data_start;
+		this->data_size = array.data_size;
+		this->front_pool = array.front_pool;
+		this->back_pool = array.back_pool;
+
+		array.data_start = 0ul;
+		array.data_size = 0ul;
+		array.front_pool = 0ul;
+		array.back_pool = 0ul;
+
+		return *this;
+	}
+
+
+	template<typename type>
+	array<type>::operator type *() const noexcept {
+		return this->data;
+	}
+
+
+}   // namespace stick
+
+
+#else
+#	warning This file is included automatically and should not be used on its own!
+#endif   //_stick_lib_data_structures_array_impl_
